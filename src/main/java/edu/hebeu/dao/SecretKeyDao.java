2c81f9caf72b6fd2222e02de93a7d1ffddd98cd1
zqr <1085620784@qq.com>
2020-09-02 10:45:41 +0800

    2020-9-1 第一版

--- src/main/java/edu/hebeu/dao/SecretKeyDao.java
+++ src/main/java/edu/hebeu/dao/SecretKeyDao.java
@@ -4,10 +4,10 @@ import org.apache.ibatis.annotations.Select;
 
 public interface SecretKeyDao {
     //查询状态为1 的状态的秘钥对的公钥
-    @Select("select tspPublicKey from secretKey where tspSecretKeyState = 1")
+    @Select("select tspPublicKey from secretkey where tspSecretKeyState = 1")
     String findPublicKey();
     //查询状态为1 的状态的秘钥对的私钥
-    @Select("select tspPrivateKey from secretKey where tspSecretKeyState = 1")
+    @Select("select tspPrivateKey from secretkey where tspSecretKeyState = 1")
     String findPrivateKey();
     //插入密钥信息
 }
